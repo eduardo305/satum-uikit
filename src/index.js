db09b578f21cb27fec509e41ea7fa42c4fa34786
Eduardo Silva <esilva2@apple.com>
2020-06-25 16:37:57 -0700

    Add feature component

--- src/index.js
+++ src/index.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import styles from './styles.module.css'
+import Feature from './components/Feature'
 import './index.css'
 
 export const ExampleComponent = ({ text }) => {
   return <div className={styles.test}>Example Component: {text}</div>
 }
+
+export { Feature }

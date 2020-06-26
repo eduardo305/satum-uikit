60e3cfe7a8fe5d690632a0224e9d6ec00f387558
Eduardo Silva <esilva2@apple.com>
2020-06-25 17:08:27 -0700

    Add fonts

--- example/src/App.js
+++ example/src/App.js
@@ -1,10 +1,16 @@
 import React from 'react'
-
-import { ExampleComponent } from 'satum-uikit'
+import { ExampleComponent, Feature } from 'satum-uikit'
 import 'satum-uikit/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return (
+    <>
+      <Feature label='Bedroom' icon='icon-bed'>
+        4
+      </Feature>
+      <ExampleComponent text='Create React Library Example 😄' />
+    </>
+  )
 }
 
 export default App
--- src/index.js
+++ src/index.js
@@ -4,7 +4,14 @@ import Feature from './components/Feature'
 import './index.css'
 
 export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+  return (
+    <div className={styles.test}>
+      Example Component: {text}
+      <Feature label='Bedroom' icon='icon-bed'>
+        4
+      </Feature>
+    </div>
+  )
 }
 
 export { Feature }
